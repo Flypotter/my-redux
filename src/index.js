c070aab9ac0d45d83a26eaa8fa9cbea33547bfc4
huzhanfeng <huzhanfeng@tal.com>
2021-07-19 12:17:56 +0800

    myRedux attempt

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, {Component} from "react";
+import Content from "./Content";
+import Header from "./Header";
+import PropTypes from 'prop-types'
+import ReactDOM from "react-dom";
+import {Provider} from './react-redux';
+
+function createStore(reducer) {
+  let state = null;
+  const listeners = [];
+  const subscribe = (listener) => listeners.push(listener);
+  const getState = () => state;
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  }
+  dispatch({});
+  return {getState, dispatch, subscribe};
+}
+
+const themeReducer = (state, action) => {
+  if (!state) {
+    return {
+      themeColor: 'red'
+    }
+  }
+  switch(action.type) {
+    case 'CHANGE_COLOR':
+      return {
+        ...state,
+        themeColor: action.themeColor
+      }
+    default:
+      return state;
+  }
+}
+
+const store = createStore(themeReducer);
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Header></Header>
+        <Content></Content>        
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <Index />
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
